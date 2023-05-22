601befc3340ea4e214a658aab2808919631d40f0
rodrigosdrigotti <rodrigosdrigotti@gmail.com>
2023-05-22 16:27:38 -0300

    Proyecto Final

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,25 +1,34 @@
 import { useState } from 'react';
 import './CheckoutForm.css';
+import { Alert } from '../Alert/Alert';
 
 export const CheckoutForm = ({ onConfirm }) => {
 
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [emailConfirm, setEmailConfirm] = useState('');
+    const [error, setError] = useState('');
 
     const handleConfirm = (event) => {
         event.preventDefault();
 
+        if(email !== emailConfirm) {
+            return setError('Error en los correos, por favor verificad ambos campos');
+        }
+
         const userData = {
             name,
             phone,
-            email
+            email,
+            emailConfirm
         }
         onConfirm(userData);
     }
 
     return (
         <div className='checkoutForm'>
+            {error && <Alert error={error} />}
             <form onSubmit={handleConfirm} className='form'>
                 <label className='label'>
                     Nombre:
@@ -45,6 +54,15 @@ export const CheckoutForm = ({ onConfirm }) => {
                         type="email"
                         value={email}
                         onChange={({target}) => setEmail(target.value)}
+                        required
+                    />
+                </label>
+                <label className='label'>
+                    Email Confirmacion:
+                    <input className='input' 
+                        type="email"
+                        value={emailConfirm}
+                        onChange={({target}) => setEmailConfirm(target.value)}
                         required
                     />
                 </label>
